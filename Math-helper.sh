550ca57c862e3c54e722e690d565f1f354f65894
jsjsjsjass <68822412+jsjsjsjass@users.noreply.github.com>
2020-11-16 17:47:13 +0200

    Add files via upload

---
+++ Math-helper.sh
@@ -0,0 +1,205 @@
+g='\033[1;32m'
+
+cyan='\033[1;36m'
+red='\033[1;31m'
+yellow='\033[1;33m'
+blue='\033[1;34m'
+p='\033[1;35m'
+
+
+ echo -e $g 'Please Wait ===+['$p'>              '$g']\'
+ sleep 0.4
+ clear
+ echo -e $cyan 'Please Wait ===+['$p'->             '$g']|'
+ sleep 0.4  
+ clear
+ echo -e $red 'Please Wait ===+['$p'-->            '$g']/'
+ sleep 0.4
+ clear
+ echo -e $yellow 'Please Wait ===+['$p'--->           '$g']\'
+ sleep 0.4
+ clear
+ echo -e $blue 'Please Wait ===+['$p'---->          '$g']|'
+ sleep 0.4
+ clear
+ echo -e $g 'Please Wait ===+['$p'----->         '$g']/'
+ sleep 0.5
+ clear
+ echo -e $red 'Please Wait ===+['$p'------>        '$g']|'
+ sleep 0.5
+ clear
+ echo -e $yellow 'Please Wait ===+['$p'------>        '$g']\'
+ sleep 0.5
+ clear
+ echo -e $blue 'Please Wait ===+['$p'------>        '$g']/'
+ sleep 0.5
+ clear
+ echo -e $cyan 'Please Wait ===+['$p'------->       '$g']|'
+ sleep 0.6
+ clear
+ echo -e $g 'Please Wait ===+['$p'-------->      '$g']/'
+ sleep 0.6
+ clear
+ echo -e $p 'Please Wait ===+['$p'--------->     '$g']\'
+ sleep 0.6
+ clear
+ echo -e $red 'Please Wait ===+['$p'---------->    '$g']|'
+ sleep 0.5
+ clear
+ echo -e $yellow 'Please Wait ===+['$p'----------->   '$g']/'
+ sleep 0.4
+ clear
+ echo -e $g 'Please Wait ===+['$p'-------------> '$g']\'
+ sleep 0.4
+ clear
+ echo -e $g 'Please Wait ===+['$p'-------------->'$g']|'
+ sleep 0.4
+ clear 
+echo -e "$yelloe+++++++++++++++>$red[Please Wait]$yellow<++++++++++++++"
+ sleep 0.4
+echo -e $red '_____   _      ______      ________  __     ______  _    _'
+sleep 0.3
+echo -e $yellow '|_   _| | |    / __ \ \    / /  ____| \ \   / / __ \| |  | |'
+sleep 0.4
+echo -e $red '  | |   | |   | |  | \ \  / /| |__     \ \_/ / |  | | |  | |
+  | |   | |   | |  | |\ \/ / |  __|     \   /| |  | | |  | |'
+sleep 0.4
+echo -e $yellow '_| |_  | |___| |__| | \  /  | |____     | | | |__| | |__| |'
+sleep 0.3
+echo -e $red '|_____| |______\____/   \/   |______|    |_|  \____/ \____/'
+sleep 5
+echo -e '
+
+
+
+
+
+'
+
+echo -e $cyan '      _4$$c_______""__ _____.$$r_          WELCOME'
+sleep 0.7
+echo -e $red '      _^$$$b____________ __e$$$"_          TO'
+sleep 0.7
+echo -e $yellow '      _d$$$$$e__MOJREM___ z$$$$$b          MY'
+sleep 0.7
+echo -e $blue '      4$$$*$$$$$c____.$$ $$$*$$$r          SCRIPT'
+sleep 0.7 
+echo -e $cyan '      _""____^*$$$be$$ $*"____"_           BE '
+sleep 0.7
+echo -e $red '      __________"$$$$" __________          ENJOY'
+sleep 0.7
+echo -e $yellow '      ________.d$$P$$$b_ ________          WHIT'
+sleep 0.7
+echo -e $blue '      _______d$$P___^$$$ b_______          CALCULAITER'
+sleep 0.7
+echo -e $cyan '      ___.ed$$$"______ "$$$be.___          MADED'
+sleep 0.7
+echo -e $red '       __$$$$$$P*Crash *$$$$$$___          BY'
+sleep 0.7
+echo -e $yellow '      4$$$$$P___Dongwan___ _$$$$$$         MOJREM'
+sleep 0.7
+echo -e $blue '      _"*$$$"_SAMIH_ __^$$P___             (samih)'
+sleep 0.7
+echo -e $cyan '      ____""__________ ____^"____          FOLLOW ME IN INST(@ou0_e)'
+sleep 2
+echo '
+
+
+
+
+'
+sleep 1
+git clone https://github.com/jsjsjsjass/Math_helper
+
+echo -e $g 'Please Wait ===+['$p'>              '$g']\'
+ sleep 0.4
+ 
+ echo -e $g 'Please Wait ===+['$p'->             '$g']|'
+ sleep 0.4  
+ 
+ echo -e $g 'Please Wait ===+['$p'-->            '$g']/'
+ sleep 0.4
+ 
+ echo -e $g 'Please Wait ===+['$p'--->           '$g']\'
+ sleep 0.4
+ 
+ echo -e $g 'Please Wait ===+['$p'---->          '$g']|'
+ sleep 0.4
+ 
+ echo -e $g 'Please Wait ===+['$p'----->         '$g']/'
+ sleep 0.5
+ 
+ echo -e $g 'Please Wait ===+['$p'------>        '$g']|'
+ sleep 0.5
+ 
+ echo -e $g 'Please Wait ===+['$p'------>        '$g']\'
+ sleep 0.5
+ 
+ echo -e $g 'Please Wait ===+['$p'------>        '$g']/'
+ sleep 0.5
+ 
+ echo -e $g 'Please Wait ===+['$p'------->       '$g']|'
+ sleep 0.6
+ 
+ echo -e $g 'Please Wait ===+['$p'-------->      '$g']/'
+ sleep 0.6
+ 
+ echo -e $g 'Please Wait ===+['$p'--------->     '$g']\'
+ sleep 0.6
+ 
+ echo -e $g 'Please Wait ===+['$p'---------->    '$g']|'
+ sleep 0.5
+ 
+ echo -e $g 'Please Wait ===+['$p'----------->   '$g']/'
+ sleep 0.4
+ 
+ echo -e $g 'Please Wait ===+['$p'-------------> '$g']\'
+ sleep 0.4
+ 
+ echo -e $g 'Please Wait ===+['$p'-------------->'$g']|'
+ sleep 0.4
+ 
+ echo -e "$g+++++++++++++++>$p[Please Wait]$g<++++++++++++++"
+ sleep 6
+echo '
+'
+sleep 0.5
+echo '
+
+'
+
+sleep 1
+
+
+echo -e $red 'WARNING!!!'
+sleep 3
+echo -e $yellow '_______________________________________________'
+sleep 3
+echo -e $red 'juste use python3 to accse in sicrpt'
+sleep 1
+echo '
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+'
+echo -e $yellow ' ENJOY AND THANKS FOR YOUR WAITING!._.'
+echo '
+'
+cd Math_helper
+ls
